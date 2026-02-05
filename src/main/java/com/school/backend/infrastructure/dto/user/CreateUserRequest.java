3e0d22088bd24a8b3ea648c04af4f23527bb6cfb
Carlos Dominguez <carlos.cfdr86@gmail.com>
2026-02-05 16:43:29 -0600

    User DTOs creation

---
+++ src/main/java/com/school/backend/infrastructure/dto/user/CreateUserRequest.java
@@ -0,0 +1,28 @@
+package com.school.backend.infrastructure.dto.user;
+
+public class CreateUserRequest {
+    private String name;
+    private String email;
+    private String role;
+    
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getRole() {
+        return role;
+    }
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    
+}
