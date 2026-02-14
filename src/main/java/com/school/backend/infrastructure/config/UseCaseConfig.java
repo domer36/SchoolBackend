01c2fd7c8a228a598868644d93855413c4c60c94
Carlos Dominguez <carlos.cfdr86@gmail.com>
2026-02-14 14:26:39 -0600

    Modify UseCaseConfig

--- src/main/java/com/school/backend/infrastructure/config/UseCaseConfig.java
+++ src/main/java/com/school/backend/infrastructure/config/UseCaseConfig.java
@@ -1,9 +1,16 @@
 package com.school.backend.infrastructure.config;
 
 import com.school.backend.application.port.UserRepository;
+import com.school.backend.application.usecase.CreateGroupUseCase;
+import com.school.backend.application.usecase.CreateStudentUseCase;
+import com.school.backend.application.usecase.CreateTeacherUseCase;
 import com.school.backend.application.usecase.CreateUserUseCase;
 import com.school.backend.application.usecase.GetUserByIdUseCase;
 import com.school.backend.application.usecase.GetUsersUseCase;
+import com.school.backend.infrastructure.persistence.repository.GroupJpaRepository;
+import com.school.backend.infrastructure.persistence.repository.StudentJpaRepository;
+import com.school.backend.infrastructure.persistence.repository.TeacherJpaRepository;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -23,5 +30,24 @@ public class UseCaseConfig {
     @Bean
     public GetUserByIdUseCase getUserByIdUseCase(UserRepository userRepository) {
         return new GetUserByIdUseCase(userRepository);
+    }
+
+    @Bean
+    public CreateGroupUseCase createGroupUseCase(
+        GroupJpaRepository groupRepository,
+        TeacherJpaRepository teacherRepository,
+        StudentJpaRepository studentRepository
+    ) {
+        return new CreateGroupUseCase(groupRepository, teacherRepository, studentRepository);
+    }
+
+    @Bean
+    public CreateTeacherUseCase createTeacherUseCase(TeacherJpaRepository teacherRepository) {
+        return new CreateTeacherUseCase(teacherRepository);
+    }
+
+    @Bean
+    public CreateStudentUseCase createStudentUseCase(StudentJpaRepository studentRepository) {
+        return new CreateStudentUseCase(studentRepository);
     }
 }
